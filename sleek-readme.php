0dd83bc9e0d3ca443063790c2519a415371ff115
powerbuoy <powerbuoy@gmail.com>
2020-03-09 10:49:33 +0100

    Better

--- sleek-readme.php
+++ sleek-readme.php
@@ -5,10 +5,10 @@ if ($path) {
 	$output = "";
 
 	foreach (glob($path) as $file) {
+		$output .= "\n---\n\n";
 		$content = file_get_contents($file);
 		$content = preg_replace('/^(#+) (.*?)$/m', '##$1 $2', $content);
 		$content = preg_replace('/^(#{7,}) (.*?)$/m', '**$2**', $content);
-		$output .= $content . "\n---\n\n";
 	}
 
 	echo $output;
