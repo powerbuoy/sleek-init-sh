a8e5caa948a114cac36562df28adb4214eb33ec9
powerbuoy <powerbuoy@gmail.com>
2020-03-09 10:56:30 +0100

    Woops

--- sleek-readme.php
+++ sleek-readme.php
@@ -9,6 +9,7 @@ if ($path) {
 		$content = file_get_contents($file);
 		$content = preg_replace('/^(#+) (.*?)$/m', '##$1 $2', $content);
 		$content = preg_replace('/^(#{7,}) (.*?)$/m', '**$2**', $content);
+		$output .= $content;
 	}
 
 	echo $output;
